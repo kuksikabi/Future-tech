c90e91be2d05ff956df0fa98bd9873c62514d636
Иван Смирнов <misterbox221@gmail.com>
2026-04-20 19:12:56 +0300

    Full Tech завершен

--- scripts/ExpandableContent.js
+++ scripts/ExpandableContent.js
@@ -1,31 +1,40 @@
+const rootSelector = "[data-js-expandable-content]";
+
 class ExpandableContent {
   selectors = {
-    root: "[data-js-expandable-content]",
-    expandedButton: "[data-js-expandable-content-button]",
+    button: "[data-js-expandable-content-button]",
     buttonText: "[data-js-icon]",
   };
+
   stateClasses = {
     isExpanded: "is-expanded",
     isClose: "is-close",
   };
+
   constructor(rootElement) {
-    this.rootElement = document.querySelector(this.selectors.root);
+    this.rootElement = rootElement;
+
     this.expandedButtonElement = this.rootElement.querySelector(
-      this.selectors.expandedButton,
+      this.selectors.button,
     );
-    this.textElement = this.expandedButtonElement.querySelector(
+    this.textElement = this.expandedButtonElement?.querySelector(
       this.selectors.buttonText,
     );
+
     this.initialText = this.textElement.textContent.trim();
     this.expandedText = "Close Blog Info";
+
     this.bindEvents();
   }
+
   onExpandedButtonClick = () => {
     this.rootElement.classList.toggle(this.stateClasses.isExpanded);
     this.expandedButtonElement.classList.toggle(this.stateClasses.isClose);
+
     const isExpanded = this.rootElement.classList.contains(
       this.stateClasses.isExpanded,
     );
+
     this.textElement.textContent = isExpanded
       ? this.expandedText
       : this.initialText;
@@ -39,4 +48,15 @@ class ExpandableContent {
   }
 }
 
-export default ExpandableContent;
+class ExpandableContentCollection {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    document.querySelectorAll(rootSelector).forEach((element) => {
+      new ExpandableContent(element);
+    });
+  }
+}
+export default ExpandableContentCollection;
